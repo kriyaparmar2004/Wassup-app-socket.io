ed7a9de5cb8adbe0968f950b4c30c6558d5871ca
kriyaparmar2004 <kriyaparmar2004@gmail.com>
2024-02-12 01:12:40 +0530

    Real time Connection established

--- server.js
+++ server.js
@@ -19,36 +19,9 @@ app.get('/',(req,res)=>{
 const io=require('socket.io')(http);
 io.on('connection', (socket) => {
     console.log('connected...');
+    socket.on('message',(msg)=>{
+        // console.log(msg)
+        socket.broadcast.emit('message',msg)
+    })
 });
-io.on('connection', (socket) => {
-    console.log('A user connected');
-});
-// const express = require('express');
-// const app = express();
-// const http = require('http').createServer(app);
-// const io = require('socket.io')(http);
 
-// const PORT = process.env.PORT || 3000;
-
-// http.listen(PORT, () => {
-//     console.log(`Listening to port ${PORT}`);
-// });
-
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', (socket) => {
-//     console.log('A user connected');
-
-//     // Example: Emit a welcome message to the connected client
-//     socket.emit('welcome', 'Welcome to the server!');
-
-//     // Example: Listen for messages from the connected client
-//     socket.on('clientMessage', (data) => {
-//         console.log('Received message from client:', data);
-//         // Process the received data or perform actions accordingly
-//     });
-// });
